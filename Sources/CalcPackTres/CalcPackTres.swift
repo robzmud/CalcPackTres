e5402f1e0c456f92f51b9b45f6d2131a8f458aed
rob <rob@rob.com>
2022-11-28 16:08:45 -0700

    Initial Commit

---
+++ Sources/CalcPackTres/CalcPackTres.swift
@@ -0,0 +1,16 @@
+public struct CalcPackTres {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
+
+public class CalculatorTres {
+    public init() {}
+    public func multiply(_ valueX: Int, with ValueY: Int) -> Int {
+        valueX * ValueY
+    }
+    public func add(_ valueX: Int, ValueY: Int) -> Int {
+        valueX + ValueY
+    }
+}
