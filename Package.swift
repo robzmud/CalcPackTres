df2ea58d63e6a886aca164368f533d8321e2bf5c
rob <rob@rob.com>
2022-11-29 08:00:41 -0700

    Adding sqlite dependency

--- Package.swift
+++ Package.swift
@@ -12,8 +12,9 @@ let package = Package(
             targets: ["CalcPackTres"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+      .package(
+        url: "https://github.com/stephencelis/SQLite.swift.git",
+        .upToNextMinor(from: "0.13.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
